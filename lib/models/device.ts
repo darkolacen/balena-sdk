6157bf7372635a675feb475dfcfab074d044be98
Thodoris Greasidis <thgreasi@gmail.com>
2020-08-08 17:44:36 +0300

    Make some typings more explicit
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- lib/models/device.ts
+++ lib/models/device.ts
@@ -229,7 +229,9 @@ const getDeviceModel = function (
 		}
 	};
 
-	const addExtraInfo = function (device: Device) {
+	const addExtraInfo = function <
+		T extends Parameters<typeof normalizeDeviceOsVersion>[0]
+	>(device: T) {
 		normalizeDeviceOsVersion(device);
 		return device;
 	};
